957cda80c77a8853e95fe2993b1c27b1ad9f7fd8
Guillaume Legrand <guillaume.legrand@payleven.de>
2013-10-26 14:51:22 +0200

    Create project structure

---
+++ client/android/src/org/graffiti/grafroid/DrawActivity.java
@@ -0,0 +1,13 @@
+package org.graffiti.grafroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DrawActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
