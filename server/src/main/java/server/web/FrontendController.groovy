0205821a63bae056c95d6bf4048817343a767fdd
Juraj Misur <juraj.misur@payleven.de>
2013-10-26 22:03:45 +0200

    streaming real svg; find graffiti works for cross-domain

--- server/src/main/java/server/web/FrontendController.groovy
+++ server/src/main/java/server/web/FrontendController.groovy
@@ -1,5 +1,7 @@
 package server.web
 
+import javax.servlet.http.HttpServletResponse
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,11 +18,13 @@ class FrontendController {
 	@Autowired
 	StorageService storage
 
-	@RequestMapping(value = "/find", method=RequestMethod.GET)
+	@RequestMapping(value = "/find", method=RequestMethod.GET, produces="application/json")
 	@ResponseBody
 	FindResponse find(@RequestParam("lat0") String lat0, @RequestParam("long0") String long0,
-			@RequestParam("lat1") String lat1, @RequestParam("long1") String long1 ){
+			@RequestParam("lat1") String lat1, @RequestParam("long1") String long1,HttpServletResponse response ){
+		response.addHeader("Access-Control-Allow-Origin", "*");
 		List<GraffitiData> data = storage.find(lat0, long0, lat1, long1)
+		println data
 		new FindResponse(data: data.collect {new GraffitiResponse(it)})
 	}
 }
