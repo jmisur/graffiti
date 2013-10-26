317b787c4bba5234e01a992d24c7ee62f59a8b44
Patrick Schinkel <schinkel@schinkelmedia.com>
2013-10-26 16:03:51 +0200

    frontend first success

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,30 @@
+var ws = null;
 
+ws = new SockJS("http://192.168.1.150:8080/echo");
+ws.onopen = function () {
+    print('Connected');
+};
+ws.onmessage = function (event) {
+    print('Received: ' + event.data);
+    var message = event.data;
+    if(pathtest==null)var pathtest = $('#realtimepaintingarea polyline').attr('points');
+    $('#realtimepaintingarea polyline').attr('points',pathtest+' '+message);
+    console.log(message);
+};
+ws.onclose = function () {
+    print('Disconnected');
+};
+
+function send() {
+    if (ws != null) {
+        var message = document.getElementById('text').value;
+        print('Sent: ' + message);
+        ws.send(message);
+    }
+}
+var pathtest = null;
+
+function print(message) {
+    var messages = document.getElementById('message');
+    messages.value += message + "\n";
+}
