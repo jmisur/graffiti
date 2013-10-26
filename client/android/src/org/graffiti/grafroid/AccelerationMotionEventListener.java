6b2eeac925eda43574337194cf308b3feea83d27
Guillaume Legrand <guillaume.legrand@payleven.de>
2013-10-26 17:50:33 +0200

    Add accessors to the Point class

--- client/android/src/org/graffiti/grafroid/AccelerationMotionEventListener.java
+++ client/android/src/org/graffiti/grafroid/AccelerationMotionEventListener.java
@@ -3,12 +3,20 @@ package org.graffiti.grafroid;
 public interface AccelerationMotionEventListener {
     
     public static class Point {
-        public final long mTimeStamp;
-        public final double mValue;
+        private final long mTimeStamp;
+        private final double mValue;
 
         public Point(final long timeStamp, final double value) {
             this.mTimeStamp = timeStamp;
             this.mValue = value;
+        }
+
+        public long getTimeStamp() {
+            return mTimeStamp;
+        }
+
+        public double getValue() {
+            return mValue;
         }
     }
     
