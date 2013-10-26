84e0c396c090e7b9357b1184f3966ee08e52e085
Guillaume Legrand <guillaume.legrand@payleven.de>
2013-10-26 17:40:32 +0200

    Make Point class immutable

--- client/android/src/org/graffiti/grafroid/AccelerationMotionEventListener.java
+++ client/android/src/org/graffiti/grafroid/AccelerationMotionEventListener.java
@@ -3,9 +3,13 @@ package org.graffiti.grafroid;
 public interface AccelerationMotionEventListener {
     
     public static class Point {
-        long mTimeStamp;
-        double mValue;
-        
+        public final long mTimeStamp;
+        public final double mValue;
+
+        public Point(final long timeStamp, final double value) {
+            this.mTimeStamp = timeStamp;
+            this.mValue = value;
+        }
     }
     
     void onMotionDownX(Point p);
